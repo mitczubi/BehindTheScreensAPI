dc064aa6a8f6788553efe06c39e9e7a24bb2c09b
Mitchell Zubich <mitchellzubich@gmail.com>
2020-01-25 22:31:23 -0500

    Prepare for deploy

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const logger = require('morgan');
 const dotenv = require('dotenv');
 
 // import routes
+const homeRoute = require('./server/routes/index');
 const mainRoutes = require('./server/routes/main');
 
 const app = express();
@@ -15,13 +16,13 @@ dotenv.config();
 
 mongoose.connect(process.env.MONGODB, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => {
-    console.log('Database connected');
   })
-  .catch((error) => {
-    console.log(error);
+  .catch(() => {
   });
 
-const port = 3000;
+mongoose.Promise = global.Promise;
+
+const port = process.env.PORT || 7066;
 
 app.get('/', (req, res) => {
   res.status(200).json({
@@ -29,8 +30,14 @@ app.get('/', (req, res) => {
   });
 });
 
+// Set up routes
+homeRoute(app)
 app.use('/api/', mainRoutes);
 
-app.listen(port, (req, res) => {
-  console.log(`Our server is live on ${port}`)
-});
+app.get('*', (req, res) => {
+  res.status(400).json({
+    message: 'This is Behing the Screens, please see documentation for proper routes.'
+  })
+})
+
+app.listen(port);
